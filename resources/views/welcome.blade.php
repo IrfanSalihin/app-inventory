14addafccaf0b94ed12f808782f3715eb9c6fb1a
IrfanSalihin <a179952@siswa.ukm.edu.my>
2023-11-21 09:49:38 +0800

    Latest

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,13 +5,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Asset Management System</title>
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap" rel="stylesheet">
     <style>
         body {
-            font-family: 'Figtree', sans-serif;
-            background: linear-gradient(135deg, #3498db, #ecf0f1);
-            color: #333;
+            font-family: 'Raleway', sans-serif;
+            background: linear-gradient(135deg, #3498db, #8e44ad);
+            color: #fff;
             text-align: center;
             margin: 0;
             padding: 0;
@@ -26,15 +26,15 @@
             padding: 2rem;
             background-color: rgba(255, 255, 255, 0.9);
             border-radius: 12px;
-            box-shadow: 0 10px 20px rgba(0, 0, 0, 0.1);
+            box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
             opacity: 0;
-            animation: fadeIn 1s ease-out forwards;
+            animation: slideIn 1s ease-out forwards;
         }
 
-        @keyframes fadeIn {
+        @keyframes slideIn {
             from {
                 opacity: 0;
-                transform: translateY(-20px);
+                transform: translateY(-50px);
             }
 
             to {
@@ -46,13 +46,11 @@
         .logo img {
             height: 120px;
             width: auto;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-            transition: transform 0.3s ease, box-shadow 0.3s ease;
+            transition: transform 0.5s ease;
         }
 
         .logo img:hover {
-            transform: scale(1.05);
-            box-shadow: 0 6px 12px rgba(0, 0, 0, 0.2);
+            transform: scale(1.1);
         }
 
         .link-buttons {
@@ -61,20 +59,21 @@
 
         .link-button {
             display: inline-block;
-            padding: 1.25rem 2rem;
+            padding: 1.5rem 3rem;
             margin: 0.5rem;
             font-size: 1.2rem;
-            font-weight: 600;
+            font-weight: 700;
             text-decoration: none;
-            border-radius: 8px;
-            transition: background-color 0.3s ease, color 0.3s ease;
-            background-color: #2980b9;
+            border-radius: 50px;
+            transition: background-color 0.3s ease, color 0.3s ease, transform 0.3s ease;
+            background-color: #3498db;
             color: #fff;
             cursor: pointer;
         }
 
         .link-button:hover {
-            background-color: #3498db;
+            background-color: #2980b9;
+            transform: scale(1.05);
         }
 
         .welcome-message {
